aefc4034bac62d3db554ec0934df173818e8e820
Thanatus <s.sidoryaka@gmail.com>
2023-04-28 13:22:32 +0300

    feat: adding keyboard layout for HTML

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable import/extensions */
+/* eslint-disable no-plusplus */
+import { keyboardKeysArr, createKeyboard } from './variables.js';
+
+// document.addEventListener('keydown', (e) => {
+//   console.log(e.code);
+// });
+
+console.log(keyboardKeysArr);
+
+createKeyboard('en');
--- js/variables.js
+++ js/variables.js
@@ -1,2 +1,545 @@
 /* eslint-disable linebreak-style */
 // eslint-disable-next-line linebreak-style
+const keyboardKeysArr = [
+  [
+    {
+      key: 'Backquote',
+      en: '`',
+      ru: 'ё',
+      upperEn: '~',
+      upperRu: 'Ё',
+    },
+    {
+      key: 'Digit1',
+      en: '1',
+      ru: '1',
+      upperEn: '!',
+      upperRu: '!',
+    },
+    {
+      key: 'Digit2',
+      en: '2',
+      ru: '2',
+      upperEn: '@',
+      upperRu: '"',
+    },
+    {
+      key: 'Digit3',
+      en: '3',
+      ru: '3',
+      upperEn: '#',
+      upperRu: '№',
+    },
+    {
+      key: 'Digit4',
+      en: '4',
+      ru: '4',
+      upperEn: '$',
+      upperRu: ';',
+    },
+    {
+      key: 'Digit5',
+      en: '5',
+      ru: '5',
+      upperEn: '%',
+      upperRu: '%',
+    },
+    {
+      key: 'Digit6',
+      en: '6',
+      ru: '6',
+      upperEn: '^',
+      upperRu: ':',
+    },
+    {
+      key: 'Digit7',
+      en: '7',
+      ru: '7',
+      upperEn: '&',
+      upperRu: '?',
+    },
+    {
+      key: 'Digit8',
+      en: '8',
+      ru: '8',
+      upperEn: '*',
+      upperRu: '*',
+    },
+    {
+      key: 'Digit9',
+      en: '9',
+      ru: '9',
+      upperEn: '(',
+      upperRu: '(',
+    },
+    {
+      key: 'Digit0',
+      en: '0',
+      ru: '0',
+      upperEn: ')',
+      upperRu: ')',
+    },
+    {
+      key: 'Minus',
+      en: '-',
+      ru: '-',
+      upperEn: '_',
+      upperRu: '_',
+    },
+    {
+      key: 'Equal',
+      en: '=',
+      ru: '=',
+      upperEn: '+',
+      upperRu: '+',
+    },
+    {
+      key: 'Backspace',
+      en: 'Backspace',
+      ru: 'Backspace',
+      upperEn: 'Backspace',
+      upperRu: 'Backspace',
+    },
+  ],
+  [
+    {
+      key: 'Tab',
+      en: 'Tab',
+      ru: 'Tab',
+      upperEn: 'Tab',
+      upperRu: 'Tab',
+    },
+    {
+      key: 'KeyQ',
+      en: 'q',
+      ru: 'й',
+      upperEn: 'Q',
+      upperRu: 'Й',
+    },
+    {
+      key: 'KeyW',
+      en: 'w',
+      ru: 'ц',
+      upperEn: 'W',
+      upperRu: 'Ц',
+    },
+    {
+      key: 'KeyE',
+      en: 'e',
+      ru: 'у',
+      upperEn: 'E',
+      upperRu: 'У',
+    },
+    {
+      key: 'KeyR',
+      en: 'r',
+      ru: 'к',
+      upperEn: 'R',
+      upperRu: 'К',
+    },
+    {
+      key: 'KeyT',
+      en: 't',
+      ru: 'е',
+      upperEn: 'T',
+      upperRu: 'Е',
+    },
+    {
+      key: 'KeyY',
+      en: 'y',
+      ru: 'н',
+      upperEn: 'Y',
+      upperRu: 'Н',
+    },
+    {
+      key: 'KeyU',
+      en: 'u',
+      ru: 'г',
+      upperEn: 'U',
+      upperRu: 'Г',
+    },
+    {
+      key: 'KeyI',
+      en: 'i',
+      ru: 'ш',
+      upperEn: 'I',
+      upperRu: 'Ш',
+    },
+    {
+      key: 'KeyO',
+      en: 'o',
+      ru: 'щ',
+      upperEn: 'O',
+      upperRu: 'Щ',
+    },
+    {
+      key: 'KeyP',
+      en: 'p',
+      ru: 'з',
+      upperEn: 'P',
+      upperRu: 'З',
+    },
+    {
+      key: 'BracketLeft',
+      en: '[',
+      ru: 'х',
+      upperEn: '{',
+      upperRu: 'Х',
+    },
+    {
+      key: 'BracketRight',
+      en: ']',
+      ru: 'ъ',
+      upperEn: '}',
+      upperRu: 'Ъ',
+    },
+    {
+      key: 'Backslash',
+      en: '\\',
+      ru: '\\',
+      upperEn: '|',
+      upperRu: '/',
+    },
+    {
+      key: 'Delete',
+      en: 'Del',
+      ru: 'Del',
+      upperEn: 'Del',
+      upperRu: 'Del',
+    },
+  ],
+  [
+    {
+      key: 'CapsLock',
+      en: 'Caps Lock',
+      ru: 'Caps Lock',
+      upperEn: 'Caps Lock',
+      upperRu: 'Caps Lock',
+    },
+    {
+      key: 'KeyA',
+      en: 'a',
+      ru: 'ф',
+      upperEn: 'A',
+      upperRu: 'Ф',
+    },
+    {
+      key: 'KeyS',
+      en: 's',
+      ru: 'ы',
+      upperEn: 'S',
+      upperRu: 'Ы',
+    },
+    {
+      key: 'KeyD',
+      en: 'd',
+      ru: 'в',
+      upperEn: 'D',
+      upperRu: 'В',
+    },
+    {
+      key: 'KeyF',
+      en: 'f',
+      ru: 'а',
+      upperEn: 'F',
+      upperRu: 'А',
+    },
+    {
+      key: 'KeyG',
+      en: 'g',
+      ru: 'п',
+      upperEn: 'G',
+      upperRu: 'П',
+    },
+    {
+      key: 'KeyH',
+      en: 'h',
+      ru: 'р',
+      upperEn: 'H',
+      upperRu: 'Р',
+    },
+    {
+      key: 'KeyJ',
+      en: 'j',
+      ru: 'о',
+      upperEn: 'J',
+      upperRu: 'О',
+    },
+    {
+      key: 'KeyK',
+      en: 'k',
+      ru: 'л',
+      upperEn: 'K',
+      upperRu: 'Л',
+    },
+    {
+      key: 'KeyL',
+      en: 'l',
+      ru: 'д',
+      upperEn: 'L',
+      upperRu: 'Д',
+    },
+    {
+      key: 'Semicolon',
+      en: ';',
+      ru: 'ж',
+      upperEn: ':',
+      upperRu: 'Ж',
+    },
+    {
+      key: 'Quote',
+      en: "'",
+      ru: 'э',
+      upperEn: '"',
+      upperRu: 'Э',
+    },
+    {
+      key: 'Enter',
+      en: 'Enter',
+      ru: 'Enter',
+      upperEn: 'Enter',
+      upperRu: 'Enter',
+    },
+  ],
+  [
+    {
+      key: 'ShiftLeft',
+      en: 'Shift',
+      ru: 'Shift',
+      upperEn: 'Shift',
+      upperRu: 'Shift',
+    },
+    {
+      key: 'KeyZ',
+      en: 'z',
+      ru: 'я',
+      upperEn: 'Z',
+      upperRu: 'Я',
+    },
+    {
+      key: 'KeyX',
+      en: 'x',
+      ru: 'ч',
+      upperEn: 'X',
+      upperRu: 'Ч',
+    },
+    {
+      key: 'KeyC',
+      en: 'c',
+      ru: 'с',
+      upperEn: 'C',
+      upperRu: 'С',
+    },
+    {
+      key: 'KeyV',
+      en: 'v',
+      ru: 'м',
+      upperEn: 'V',
+      upperRu: 'М',
+    },
+    {
+      key: 'KeyB',
+      en: 'b',
+      ru: 'и',
+      upperEn: 'B',
+      upperRu: 'И',
+    },
+    {
+      key: 'KeyN',
+      en: 'n',
+      ru: 'т',
+      upperEn: 'N',
+      upperRu: 'Т',
+    },
+    {
+      key: 'KeyM',
+      en: 'm',
+      ru: 'ь',
+      upperEn: 'M',
+      upperRu: 'Ь',
+    },
+    {
+      key: 'Comma',
+      en: ',',
+      ru: 'б',
+      upperEn: '<',
+      upperRu: 'Б',
+    },
+    {
+      key: 'Period',
+      en: '.',
+      ru: 'ю',
+      upperEn: '>',
+      upperRu: 'Ю',
+    },
+    {
+      key: 'Slash',
+      en: '/',
+      ru: '.',
+      upperEn: '?',
+      upperRu: ',',
+    },
+    {
+      key: 'ArrowUp',
+      en: '↑',
+      ru: '↑',
+      upperEn: '↑',
+      upperRu: '↑',
+    },
+    {
+      key: 'ShiftRight',
+      en: 'Shift',
+      ru: 'Shift',
+      upperEn: 'Shift',
+      upperRu: 'Shift',
+    },
+  ],
+  [
+    {
+      key: 'ControlLeft',
+      en: 'Ctrl',
+      ru: 'Ctrl',
+      upperEn: 'Ctrl',
+      upperRu: 'Ctrl',
+    },
+    {
+      key: 'MetaLeft',
+      en: 'Win',
+      ru: 'Win',
+      upperEn: 'Win',
+      upperRu: 'Win',
+    },
+    {
+      key: 'AltLeft',
+      en: 'Alt',
+      ru: 'Alt',
+      upperEn: 'Alt',
+      upperRu: 'Alt',
+    },
+    {
+      key: 'Space',
+      en: ' ',
+      ru: ' ',
+      upperEn: ' ',
+      upperRu: ' ',
+    },
+    {
+      key: 'AltRight',
+      en: 'Alt',
+      ru: 'Alt',
+      upperEn: 'Alt',
+      upperRu: 'Alt',
+    },
+    {
+      key: 'ArrowLeft',
+      en: '←',
+      ru: '←',
+      upperEn: '←',
+      upperRu: '←',
+    },
+    {
+      key: 'ArrowDown',
+      en: '↓',
+      ru: '↓',
+      upperEn: '↓',
+      upperRu: '↓',
+    },
+    {
+      key: 'ArrowRight',
+      en: '→',
+      ru: '→',
+      upperEn: '→',
+      upperRu: '→',
+    },
+    {
+      key: 'ControlRight',
+      en: 'Ctrl',
+      ru: 'Ctrl',
+      upperEn: 'Ctrl',
+      upperRu: 'Ctrl',
+    },
+  ],
+];
+
+function createKeyboard(lang) {
+  const body = document.querySelector('body');
+  const wrapper = document.createElement('div');
+  const wrapperKeyboard = document.createElement('div');
+  const h1 = document.createElement('h1');
+  const h4 = document.createElement('h4');
+  const keyboardDisplay = document.createElement('textarea');
+  const helpInfo = document.createElement('div');
+  const keyboardKeys = document.createElement('keyboard__keys');
+
+  keyboardDisplay.name = 'text';
+  keyboardDisplay.id = 'keyboard__display';
+  keyboardDisplay.cols = 50;
+  keyboardDisplay.rows = 5;
+
+  helpInfo.className = 'help-info';
+  wrapper.className = 'wrapper';
+  wrapperKeyboard.className = 'wrapper__keyboard';
+  h1.innerText = 'RSS Виртуальная клавиатура';
+  h4.innerHTML = `Клавиатура создана в операционной системе Windows
+                  <br>
+                  Для переключения языка комбинация: левыe ctrl + alt`;
+  helpInfo.append(h4);
+  wrapperKeyboard.append(h1);
+  wrapperKeyboard.append(keyboardDisplay);
+  wrapper.append(wrapperKeyboard);
+  body.append(wrapper);
+  keyboardKeys.className = 'keyboard__keys';
+
+  for (let i = 0; i < keyboardKeysArr.length; i++) {
+    const rowKeys = document.createElement('div');
+    rowKeys.className = 'row-keys';
+    keyboardKeysArr[i].forEach((el) => {
+      const key = document.createElement('div');
+      if (el.key === 'Backspace') {
+        key.className = 'functional backspace';
+      }
+      if (el.key === 'Tab') {
+        key.className = 'functional tab';
+      }
+      if (el.key === 'Delete') {
+        key.className = 'functional del';
+      }
+      if (el.key === 'Enter') {
+        key.className = 'functional enter';
+      }
+      if (el.key === 'CapsLock') {
+        key.className = 'functional capslock';
+      }
+      if (el.key === 'ShiftLeft') {
+        key.className = 'functional left-shift';
+      }
+      if (el.key === 'ShiftRight') {
+        key.className = 'functional right-shift';
+      }
+      if (el.key === 'MetaLeft') {
+        key.className = 'functional win';
+      }
+      if (el.key === 'AltLeft' || el.key === 'AltRight') {
+        key.className = 'functional win';
+      }
+      if (el.key === 'ControlLeft' || el.key === 'ControlRight') {
+        key.className = 'functional ctrl';
+      }
+      if (el.key === 'ArrowUp' || el.key === 'ArrowDown' || el.key === 'ArrowLeft' || el.key === 'ArrowRight') {
+        key.className = 'functional arrow';
+      }
+      if (el.key === 'Space') {
+        key.className = 'space';
+      }
+      key.classList.add('key');
+      key.innerText = el[lang];
+      key.dataset.code = el.key;
+      rowKeys.append(key);
+    });
+    keyboardKeys.append(rowKeys);
+  }
+  wrapperKeyboard.append(keyboardKeys);
+  wrapperKeyboard.append(helpInfo);
+}
+
+export { keyboardKeysArr, createKeyboard };
